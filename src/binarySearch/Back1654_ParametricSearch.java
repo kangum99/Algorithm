2c3c38ae7f6b1a9a10527b3060965d14986d827b
yuum <kangum99@gmail.com>
2021-02-26 16:55:48 +0900

    랜선자르기-이분탐색 -> 다 못품

---
+++ src/binarySearch/Back1654_ParametricSearch.java
@@ -0,0 +1,21 @@
+package binarySearch;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//https://www.acmicpc.net/problem/1654 - 랜선자르기 -이분탐색, parametric search
+
+
+public class Back1654_ParametricSearch {
+	static int k, n;
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		k = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+	}
+
+}
