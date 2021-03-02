aea5ea994cd9cb99c5ca0cedda605f8adbda4758
yuum <kangum99@gmail.com>
2021-03-02 16:42:38 +0900

    프로그래머스-정렬-가장 큰수-주석달기

--- src/sort/Pro_가장큰수.java
+++ src/sort/Pro_가장큰수.java
@@ -10,9 +10,11 @@ public class Pro_가장큰수 {
 	static String solution(int[] numbers) {
 		String[] s = new String[numbers.length];
 		for(int i = 0; i < numbers.length; i++) {
+//			string으로 변환
 			s[i] = String.valueOf(numbers[i]);
 		}
 		Arrays.sort(s, new Comparator<String>() {
+//			오버라이드 해서 원하는 기준으로 바꿔줌
 			@Override
 			public int compare(String o1, String o2) {
 				// TODO Auto-generated method stub
@@ -20,7 +22,8 @@ public class Pro_가장큰수 {
 				return (o2+o1).compareTo(o1+o2);
 			}
 		});
-		
+//		0 0 만 들어 있는 경우 는 0 하나만 적어주기 
+//		아닌경우 join
 		return (s[0].equals("0")? s[0] : String.join("", s));
 	}
 
